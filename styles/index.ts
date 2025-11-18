1551148b3515470dee77992c578610dbd8232826
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2025-11-18 15:32:26 +0100

    feat: add styles

---
+++ styles/index.ts
@@ -0,0 +1,7 @@
+// inherit from base layouts, remove it to get full customizations
+import '@slidev/client/styles/layouts-base.css'
+
+// PrimeIcons
+import 'primeicons/primeicons.css'
+
+import './layout.css'
